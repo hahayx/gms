af0683a486515c56f050858be8ba2e8ea1ed3426
unknown <liuchunjiang@rd.com>
2016-04-29 17:58:04 +0800

    3

---
+++ gms/src/com/hh/common/servlet/GmsListener.java
@@ -0,0 +1,20 @@
+package com.hh.common.servlet;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class GmsListener implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		
+		
+	}
+
+}
---
+++ gms/src/com/hh/gms/web/GmsController.java
@@ -0,0 +1,16 @@
+package com.hh.gms.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+
+public class GmsController {
+
+	@RequestMapping("/index.html")
+	public ModelAndView index() {
+		
+		return new ModelAndView("index");
+	}
+}
